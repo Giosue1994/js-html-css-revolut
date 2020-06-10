b355874ccb98859b717a1c9fd922144190973274
Giosue1994 <giosuelentini1994@outlook.com>
2020-06-10 19:58:16 +0200

    aggiunti dettagli

--- js/script.js
+++ js/script.js
@@ -10,6 +10,16 @@ $(document).ready(
       }
     );
 
+    // quando passo col mouse sopra un elemento della lista che ha
+    // id 'lang' rimuovo la classe 'fa-angle-down' e aggiungo
+    // la classe 'fa-angle-up'
+    $('#lang').mouseenter(
+      function() {
+        $('i').removeClass('fa-angle-down');
+        $('i').addClass('fa-angle-up');
+      }
+    );
+
 
     // quando tolgo il mouse dall'elemento della lista che ha classe
     // 'dropdown' la classe 'active' viene rimossa e il menu viene nascosto
@@ -19,11 +29,31 @@ $(document).ready(
       }
     );
 
+    // quando passo col mouse sopra un elemento della lista che ha
+    // id 'lang' rimuovo la classe 'fa-angle-up' e aggiungo
+    // la classe 'fa-angle-down'
+    $('#lang').mouseleave(
+      function() {
+        $('i').removeClass('fa-angle-up');
+        $('i').addClass('fa-angle-down');
+      }
+    );
+
     // quando clicco su un elemento della lista che ha classe
     // 'dropdown' la classe 'active' viene rimossa e il menu viene nascosto
     $('.with-dropdown').click(
       function() {
         $(this).children('.dropdown').removeClass('active');
+      }
+    );
+
+    // quando clicco su un elemento della lista che ha
+    // id 'lang' rimuovo la classe 'fa-angle-up' e aggiungo
+    // la classe 'fa-angle-down'
+    $('#lang').click(
+      function() {
+        $('i').removeClass('fa-angle-up');
+        $('i').addClass('fa-angle-down');
       }
     );
 
